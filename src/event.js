3ea245f3973dffa375c6234e57010127350b1b93
octopod <octopodsquad@gmail.com>
2017-03-04 14:33:33 -1000

    Split event.js into identifiable.js and event.js

--- src/event.js
+++ src/event.js
@@ -1,39 +1,41 @@
 
-var Options = require("./util/options");
+//var Options = require("./util/options");
+var Identifiable = require("./identifiable");
+
 /**
  * Represents something that happens throughout a tournament. For example,
  * matches or rating algorithm changes can be considered an event.
  *
  * All events should have the id and time of the event as a property.
  */
-class Event {
-	constructor(obj) { this.__verifyObject(obj);
+class Event extends Identifiable {
+	constructor(options = {}) { super(options);
+		if(isNaN(options.time) || options.time < 0) {
+			throw new RangeError("The provided time for this event is invalid: " + options.time);
+		}
 		/**
 		 * The identifier for the event.
 		 * @type {string}
 		 */
-		Object.defineProperty(this, "id", {value: obj.id});
+		Object.defineProperty(this, "id", {value: options.id});
 		/**
 		 * The time of the event, in UNIX.
 		 * @type {number}
 		 */
-		Object.defineProperty(this, "time", {value: obj.time});
+		Object.defineProperty(this, "time", {value: options.time});
 	}
 
-	__verifyObject(obj) {
-		obj = Options.merge({
-			type: null, // the type of event
-			id:   null, // the id of this event
-			time: null  // the time this event occured
-		}, obj);
-
-		if(obj.id == null) {
-			throw new ReferenceError("The ID cannot be null.");
-		}
-		if(isNaN(obj.time) || obj.time < 0) {
-			throw new RangeError("The provided time for this event is invalid: " + obj.time);
-		}
-	}
+	// __verifyObject(options) {
+	// 	options = Options.merge({
+	// 		type: null, // the type of event
+	// 		id:   null, // the id of this event
+	// 		time: null  // the time this event occured
+	// 	}, options);
+	//
+	// 	if(isNaN(options.time) || options.time < 0) {
+	// 		throw new RangeError("The provided time for this event is invalid: " + options.time);
+	// 	}
+	// }
 }
 
 module.exports = Event;
--- src/identifiable.js
+++ src/identifiable.js
@@ -1,6 +1,19 @@
 
+/**
+ * Represents an object that can be identified by a string or a number.
+ */
 class Identifiable {
-	constructor(id) { this.id = id; }
+	constructor(options = {}) {
+		if(!(typeof options.id == "string") && !(typeof options.id == "number")) {
+			throw new TypeError("ID of object must be either a string or a number.");
+		}
+
+		/**
+		 * The identifier for this object.
+		 * @type {string} | {number}
+		 */
+		Object.defineProperty(this, "id", {value: options.id});
+	}
 }
 
 module.exports = Identifiable;
