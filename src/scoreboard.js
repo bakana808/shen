be62e90bb8a1e4b9018d8427c80e2ec4bbc22340
Branden Akana <branden.akana@gmail.com>
2019-04-20 16:28:00 -1000

    Restructure Scoreboard to use as a Map for scores

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -1,35 +1,119 @@
 
+/**
+ * Keeps track of user scores in a contest.
+ *
+ */
 class Scoreboard {
-	constructor(scoreMap, winners) {
-		this._scoreMap = scoreMap;
-		this._winners = winners;
-	}
+	
+	/**
+	 * Creates a new scoreboard.
+	 *
+	 * @param {Collection<User>} users
+	 */
+	constructor(users) {
 
-	static create(scoreMap, fn) {
-		var winners = [];
-		scoreMap.forEach((userid, score) => {
-			if(fn(userid, score)) { winners.push(userid); }
+		if(!Array.isArray(users)) {
+
+			throw new Error("an array of users must be provided to create a scoreboard");
+		}
+
+		Object.defineProperty(this, "users", { value: users });
+
+		this.scores = new Map();
+
+		users.forEach(user => {
+
+			this.scores.set(user.id, 0);
 		});
-		return new Scoreboard(scoreMap, winners);
 	}
 
-	get winners() { return this._winners; }
+	setScore(user, score) {
 
-	getScore(userid) {
-		if(!this._scoreMap.has(userid)) {
-			throw new ReferenceError("this player does not exist in this scoreboard.");
-		} else {
-			return this._scoreMap.get(userid);
+		if(!this.scores.has(user.id)) {
+
+			throw new Error("cannot set score of user not on this scoreboard");
+		}
+
+		if(isNaN(score) || score < 0 || score > 1) {
+
+			throw new Error("score out of range; must be between 0 and 1");
+		}
+
+		this.scores.set(user.id, score);
+	}
+
+	setWinner(user, score) {
+
+		this.scores.forEach((score, id) => {
+
+			if(id == user.id) {
+
+				this.scores.set(id, 1);
+			}
+			else {
+				
+				this.scores.set(id, 0);
+			}
+		});
+	}
+
+	getScore(user) {
+
+		if(!this.scores.has(user.id)) {
+
+			throw new Error("cannot get score of user not on this scoreboard");
+		}
+
+		return this.scores.get(user.id);
+	}
+
+	getScoreArray() {
+
+		return this.scores.values();
+	}
+
+	/**
+	 * Returns the user with the highest score.
+	 *
+	 * @returns {User} The user with the highest score
+	 */
+	getWinner() {
+
+		var user = null;
+		var score = 0;
+
+		this.scores.forEach((v, k) => {
+
+			if(v > score) {
+
+				user = this.users.get(k);
+				score = v;
+			}
+		});
+
+		if(score == 0) {
+
+			throw new Error("cannot get highest; all scores are 0");
 		}
 	}
 
-	getWinner() { return this.winners[0]; }
+	/**
+	 * Returns true if there is at least one winner.
+	 *
+	 * @returns {boolean}
+	 */
+	isResolved() {
 
-	isWinner(userid) { return this.winners.includes(userid); }
+		try {
+			
+			this.getWinner();
+			return true;
+		}
+		catch (e) {
 
-	isTie() { return this.winners.length > 1; }
-
-	hasWinners() { return this._winners.length > 0; }
+			return false;
+		}
+	}
 }
 
 module.exports = Scoreboard;
